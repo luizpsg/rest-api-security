cdf9fa10da6d5e89f3ff760e2005a64b55dc801e
luizpsg <999785@sga.pucminas.br>
2024-04-21 19:33:43 -0300

    Update DemoSecurityConfig to configure HTTP security and request authorization

--- src/main/java/com/luizpsg/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/luizpsg/cruddemo/security/DemoSecurityConfig.java
@@ -2,9 +2,13 @@ package com.luizpsg.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -33,4 +37,22 @@ public class DemoSecurityConfig {
     return new InMemoryUserDetailsManager(john, mary, susan);
   }
 
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    
+    http.authorizeHttpRequests( configurer -> configurer
+        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+        .requestMatchers(HttpMethod.PUT, "/api/employees/").hasRole("MANAGER")
+        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+    );
+
+    http.httpBasic(Customizer.withDefaults());
+
+    http.csrf( csrf -> csrf.disable() );
+
+    return http.build();
+  }
 }
