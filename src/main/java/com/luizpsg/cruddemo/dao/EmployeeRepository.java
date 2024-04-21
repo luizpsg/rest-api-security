e1614259aa860411334f1bbcfa365503bc1b103e
luizpsg <999785@sga.pucminas.br>
2024-04-21 19:03:29 -0300

    Add EmployeeService, EmployeeRepository, EmployeeServiceImpl, and EmployeeRestController classes

--- src/main/java/com/luizpsg/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/luizpsg/cruddemo/dao/EmployeeRepository.java
@@ -1,11 +1,11 @@
 package com.luizpsg.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.luizpsg.cruddemo.entity.Employee;
 
-@RepositoryRestResource(path = "members")
-public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
 
 }
---
+++ src/main/java/com/luizpsg/cruddemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.luizpsg.cruddemo.service;
+
+import java.util.List;
+
+import com.luizpsg.cruddemo.entity.Employee;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
